7c301dc5988eab78f4cf731b22be08b4822b4354
otallyto <rodrigues.tallyto@hotmail.com>
2020-11-19 22:31:36 -0400

    chore: run lint

--- src/util/getCurrency.js
+++ src/util/getCurrency.js
@@ -1,22 +1,22 @@
 const axios = require('axios');
+
 const currencyAPI = 'https://economia.awesomeapi.com.br/json/all';
 
 const getCurrency = async (currency) => {
-    const currencys = ['USD', 'USDT', 'CAD', 'AUD', 'EUR', 'GBP', 'ARS', 'JPY', 'CHF', 'CNY', 'YLS', 'JPY', 'CHF', 'BTC', 'LTC', 'ETH', 'XRP']
-    const options = {
-        style: 'currency',
-        currency: 'BRL'
-    }
+  const currencys = ['USD', 'USDT', 'CAD', 'AUD', 'EUR', 'GBP', 'ARS', 'JPY', 'CHF', 'CNY', 'YLS', 'JPY', 'CHF', 'BTC', 'LTC', 'ETH', 'XRP'];
+  const options = {
+    style: 'currency',
+    currency: 'BRL',
+  };
 
-    if(currencys.includes(currency.toUpperCase())) {
-        const response = await axios.get(`${currencyAPI}/${currency}-BRL`)
-        const { high, name } = response.data[currency.toUpperCase()]
+  if (currencys.includes(currency.toUpperCase())) {
+    const response = await axios.get(`${currencyAPI}/${currency}-BRL`);
+    const { high, name } = response.data[currency.toUpperCase()];
 
-        return `${name} - ${Number(high).toLocaleString('pt-BR', options)} 💸` 
-    }
+    return `${name} - ${Number(high).toLocaleString('pt-BR', options)} 💸`;
+  }
 
-    return 'Comando inválido. Para saber os comandos válidos, digite:\n\n/currency help'
+  return 'Comando inválido. Para saber os comandos válidos, digite:\n\n/currency help';
+};
 
-}
-
-module.exports = getCurrency
+module.exports = getCurrency;
