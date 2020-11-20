14eabc5e5654a9d84a2de1106246ac675b654b40
otallyto <rodrigues.tallyto@hotmail.com>
2020-11-19 21:58:36 -0400

    feat:  search cep

---
+++ src/commands/cep.ts
@@ -0,0 +1,13 @@
+import cepPromisse from 'cep-promise'
+
+export default async function (bot: any, msg: any, match: any) {
+  try {
+    const response = await cepPromisse(match)
+    const { street, neighborhood, city, state, } = response
+
+    bot.sendMessage(msg.chat.id, `${street}, ${neighborhood},  ${city} - ${state}`)
+  } catch (error) {
+    bot.sendMessage(msg.chat.id, 'cep não encontrado')
+  }
+}
+
